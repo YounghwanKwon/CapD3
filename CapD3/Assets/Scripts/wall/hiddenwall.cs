22fd083df25c3c8f2563b25fd651bd0df884ca19
scullystory <kyh0124.12@gmail.com>
2019-09-17 18:01:41 +0900

    맵 1개생성 ( 실수로 오리지날 맵이 수정됨 ) + 벽트리거 생성

---
+++ CapD3/Assets/Scripts/wall/hiddenwall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hiddenwall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            Debug.Log("플레이어가 벽에 도달하였습니다");
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
