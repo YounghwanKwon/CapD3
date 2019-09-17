eb0c1423e1a92c5c76ac6d48350d6ae5ba0cd5f8
scullystory <kyh0124.12@gmail.com>
2019-09-18 04:43:28 +0900

    벽에 충돌시 특정오브젝트 소환하는 코드 hiddenwall.cs

--- CapD3/Assets/Scripts/wall/hiddenwall.cs
+++ CapD3/Assets/Scripts/wall/hiddenwall.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class hiddenwall : MonoBehaviour
 {
+    [SerializeField] private GameObject tank;
+    [SerializeField] private Transform spwanpoint;
+    [SerializeField] private GameObject HC;
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
         if (other.transform.CompareTag("Player"))
         {
             Debug.Log("플레이어가 벽에 도달하였습니다");
+            Debug.Log("탱크소환을 시도합니다");
+            Instantiate(tank, spwanpoint.position, spwanpoint.rotation);
+            //tank.Setup();
+            //HC = Instantiate(HC, spwanpoint, spwanpoint.rotation);
         }
+
     }
     void Start()
     {
