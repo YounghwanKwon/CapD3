a2cbeb77bb45c1860a7b46ea8fc0a217a29cf078
scullystory <kyh0124.12@gmail.com>
2019-09-18 06:52:34 +0900

    약 정상소모확인

---
+++ CapD3/Assets/_Completed-Assets/Scripts/HealingTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Complete
+{
+
+    public class HealingTest : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        [SerializeField] private float healingamount = 15.0f;
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.transform.CompareTag("Player"))
+            {
+                Debug.Log("유저충돌감지");
+                Rigidbody targetRigidbody = other.GetComponent<Rigidbody>();
+
+                TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
+                targetHealth.GetHealing(healingamount);
+
+
+                Destroy(this.gameObject);
+                regen();
+
+            }
+        }
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            spin();
+        }
+
+        void spin()
+        {
+            this.transform.rotation = new Quaternion(0, this.transform.rotation.y + 5, 0, 0);
+        }
+
+        void regen()
+        {
+
+        }
+    }
+}
