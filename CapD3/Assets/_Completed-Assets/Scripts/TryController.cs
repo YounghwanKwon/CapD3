d7c2b77168fa802ad86f82c955127143005c04bc
scullystory <kyh0124.12@gmail.com>
2019-09-23 21:24:47 +0900

    컨트롤러 작동은함 다만 회전과 원하는 이동방향이 일치하지않음

---
+++ CapD3/Assets/_Completed-Assets/Scripts/TryController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TryController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    [SerializeField] private RectTransform rect_Background;
+    [SerializeField] private RectTransform rect_Joystic;
+
+    private float radius;
+
+    [SerializeField] private GameObject go_Player;
+    [SerializeField] private float movespeed;
+
+    private bool isTouch = false;
+    private Vector3 movePosition;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius = rect_Background.rect.width * 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTouch)
+        {
+            go_Player.transform.position += movePosition;
+        }
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 value = eventData.position - (Vector2)rect_Background.position;
+
+        value = Vector2.ClampMagnitude(value, radius);
+        rect_Joystic.localPosition = value;
+
+        float distance = Vector2.Distance(rect_Background.position, rect_Joystic.position) / radius;
+        Debug.Log(distance);
+        value = value.normalized;
+        movePosition = new Vector3(value.x * movespeed * distance * Time.deltaTime, 0f, value.y * movespeed * distance * Time.deltaTime);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isTouch = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isTouch = false;
+        rect_Joystic.localPosition = Vector3.zero;
+        //movePosition = Vector3.zero;
+    }
+}
