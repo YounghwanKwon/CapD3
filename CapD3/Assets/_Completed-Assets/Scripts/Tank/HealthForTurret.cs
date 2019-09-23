0fc376c4540f569db8af8f843c47fc7c8c84140d
scullystory <kyh0124.12@gmail.com>
2019-09-23 15:43:04 +0900

    터렛에 색칠

--- CapD3/Assets/_Completed-Assets/Scripts/Tank/HealthForTurret.cs
+++ CapD3/Assets/_Completed-Assets/Scripts/Tank/HealthForTurret.cs
@@ -126,5 +126,14 @@ public class HealthForTurret : MonoBehaviour
     private void Start()
     {
         originpos = this.transform;
+
+        MeshRenderer[] renderers = GetComponents<MeshRenderer>();
+
+        // Go through all the renderers...
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            // ... set their material color to the color specific to this tank.
+            renderers[i].material.color = new Color(1, 0, 0, 1);
+        }
     }
 }
