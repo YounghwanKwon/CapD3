86a0be9f934597162f4ba7c40bba4b1f2782af2b
yoondohyun <ydh3434@daum.net>
2019-09-26 20:02:58 +0900

    Merge branch 'for_tuesday_better' of https://github.com/YounghwanKwon/CapD3 into firebtnend
    # Conflicts:
    #	CapD3/Assets/Scenes/Main.unity

---
+++ CapD3/Assets/MenuButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public bool PauseBtn = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PauseBtn)
+        {
+            Time.timeScale = 0;
+            return;
+        }
+        if(!PauseBtn)
+        {
+            Time.timeScale = 1;
+            return;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        PauseBtn = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        PauseBtn = false;
+    }
+}
