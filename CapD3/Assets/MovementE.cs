47188af20ad0326da7080bbfd1485a40ea862501
yoondohyun <ydh3434@daum.net>
2019-09-29 17:06:17 +0900

    적 이동 성공

---
+++ CapD3/Assets/MovementE.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovementE : MonoBehaviour
+{
+    public GameObject player;
+    public NavMeshAgent nav;
+    public GameObject MovePoint1;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        nav = GetComponent<NavMeshAgent>();
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(MovePoint1);
+        Debug.Log(MovePoint1.transform.position);
+        Debug.Log(nav);
+        nav.SetDestination(MovePoint1.transform.position);
+        //nav.SetDestination(player.transform.position);
+    }
+}
