f546358ddd844e0edd893ef519f7fc64afc87be3
yoondohyun <ydh3434@daum.net>
2019-10-01 08:11:32 +0900

    좌측 맵 미완성

---
+++ CapD3/Assets/WallCube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCube : MonoBehaviour
+{
+    public GameObject BallBomb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            this.gameObject.SetActive(false);
+            BallBomb.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("버그");
+        }
+    }
+}
