eab0fd0dd123a51fd32c87a914609738f54a9559
scullystory <kyh0124.12@gmail.com>
2019-09-30 21:09:15 +0900

    여기서부터 디바이드

---
+++ CapD3/Assets/Lab2ManagerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lab2ManagerScript : MonoBehaviour
+{
+    [SerializeField] private GameObject shell1;
+    [SerializeField] private Transform BombPlacingPoint1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DropA();
+    }
+    void DropA()
+    {
+        Instantiate(shell1, BombPlacingPoint1);
+        //Instantiate(shell1,new Vector3(0,0.1f,0),Quaternion.Euler(Vector3.zero));
+        Invoke("DropA", 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
