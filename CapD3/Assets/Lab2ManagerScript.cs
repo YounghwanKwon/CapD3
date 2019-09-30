0a5ceab74e66c128024841009d38d2bc5c1eba43
yoondohyun <ydh3434@daum.net>
2019-10-01 00:41:37 +0900

    원형방 개발중

--- CapD3/Assets/Lab2ManagerScript.cs
+++ CapD3/Assets/Lab2ManagerScript.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class Lab2ManagerScript : MonoBehaviour
 {
     [SerializeField] private GameObject shell1;
+    [SerializeField] private GameObject shell2;
+
     [SerializeField] private Transform BombPlacingPoint1;
+    [SerializeField] private Transform BombPlacingPoint2;
+    [SerializeField] private Transform BombPlacingPoint3;
+    [SerializeField] private Transform BombPlacingPoint4;
+    [SerializeField] private Transform BombPlacingPoint5;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +20,10 @@ public class Lab2ManagerScript : MonoBehaviour
     void DropA()
     {
         Instantiate(shell1, BombPlacingPoint1);
+        Instantiate(shell2, BombPlacingPoint2);
+        Instantiate(shell2, BombPlacingPoint3);
+        Instantiate(shell2, BombPlacingPoint4);
+        Instantiate(shell2, BombPlacingPoint5);
         //Instantiate(shell1,new Vector3(0,0.1f,0),Quaternion.Euler(Vector3.zero));
         Invoke("DropA", 2);
     }
---
+++ CapD3/Assets/TurnObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnObject : MonoBehaviour
+{
+    public GameObject Lturret;
+    public Vector3 RotVec;
+    public Vector3 RotSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        RotSpeed = new Vector3(0, 1f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotVec += RotSpeed;
+        Lturret.transform.rotation = Quaternion.Euler(RotVec);
+    }
+}
