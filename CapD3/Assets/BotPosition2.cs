398060a864c5f279030da0e2ce2d3a7f36ed92d1
yoondohyun <ydh3434@daum.net>
2019-10-03 00:01:19 +0900

    패트롤봇 만드는중

---
+++ CapD3/Assets/BotPosition2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotPosition2 : MonoBehaviour
+{
+    public static bool posstatus2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posstatus2 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter()
+    {
+        Debug.Log("position2 test");
+        posstatus2 = true;
+        BotPosition1.posstatus1 = false;
+    }
+}
---
+++ CapD3/Assets/SightTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SightTrigger : MonoBehaviour
+{
+    public GameObject Sight;
+    public static bool SightCat;
+    public GameObject Lighting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SightCat = false;
+        InvokeRepeating("OnTriggerEnter", 0f, 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Tank_e1")
+        {            
+            SightCat = true;
+            Lighting.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("에러남");
+        }
+    }
+}
