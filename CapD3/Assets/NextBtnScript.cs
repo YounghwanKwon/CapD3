cba4dc3d7dee9f9e7154c6ed7c409b9faca080f1
scullystory <kyh0124.12@gmail.com>
2019-10-05 22:52:57 +0900

    소스 코드 수정 필요없는 부분삭제

--- CapD3/Assets/NextBtnScript.cs
+++ CapD3/Assets/NextBtnScript.cs
@@ -5,96 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class NextBtnScript : MonoBehaviour
 {
-    [SerializeField] private GameObject newtestmap;
-    [SerializeField] private GameObject newtesttank;
-    [SerializeField] private GameObject newHP;
-
-
-    [SerializeField] private GameObject oldCP;
-    [SerializeField] private GameObject oldGC;
-    [SerializeField] private GameObject oldtank;
-    [SerializeField] private GameObject timer;
-    [SerializeField] private GameObject exitBtn;
-    private int stagenumber;
+    
+    //private int stagenumber;
     //[SerializeField] private GameObject completedtuesdaymap;
     // Start is called before the first frame update
 
     public void whennextbtnpressed()
     {
-        Debug.Log(stagenumber);
         Time.timeScale = 1;
-        if (StageSaveScript.StageNum == 1)
+        if (StageSaveScript.StageNum == 0)
+            SceneManager.LoadScene("stage1");
+        else if (StageSaveScript.StageNum == 1)
             SceneManager.LoadScene("stage2");
         else if (StageSaveScript.StageNum == 2)
             SceneManager.LoadScene("stage3");
-        else if (StageSaveScript.StageNum == 0)
-            SceneManager.LoadScene("stage1");
         else if (StageSaveScript.StageNum == 3)
             SceneManager.LoadScene("Lab2");
         else
-            Debug.Log("망함.. stagenumber : " + stagenumber);
-        //Debug.Log("reset버튼이 눌림");
-        //ExitScript escscript = exitBtn.GetComponent<ExitScript>();
-        //escscript.countup();
-        //tryreset();
-    }
-    public void setstage(int number)
-    {
-        Time.timeScale = 1;
-        stagenumber = number;
-    }
-
-    public void whenStageSelectBtnpressed()
-    {
-        Time.timeScale = 1;
-        SceneManager.LoadScene("StageSelect");
-    }
-    void tryreset()
-    {
-        oldtank.transform.position = new Vector3(0, 0, 0);
-        oldtank.transform.rotation = Quaternion.Euler(0, 90, 0);//탱크 재배치
-        Complete.TankHealth temptank = oldtank.GetComponent<Complete.TankHealth>();//탱크 체력회복
-        temptank.fullrecovery();
-
-        oldGC.SetActive(true); //게임코어 재배치
-
-        CapturePoint tempCP = oldCP.GetComponent<CapturePoint>();
-        tempCP.CPreset(); //거점 점령시간초기화
-
-        Complete.TankShooting temptankshooting = oldtank.GetComponent<Complete.TankShooting>();
-        temptankshooting.refillbullet(); //총알 재보급
-
-        TimerScript temptimer = timer.GetComponent<TimerScript>(); //시간 0초부터 다시재생
-        temptimer.timezerofy();
-        temptimer.timepasson();
-
-        this.gameObject.SetActive(false);
-        /*
-        destorypart();
-        instantiatepart();
-        movingpart();
-        */
-    }
-    void destorypart()
-    {
-
-    }
-    void instantiatepart()
-    {
-
-    }
-    void movingpart()
-    {
-
-    }
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+            Debug.Log("망함.. stagenumber : " + StageSaveScript.StageNum);
     }
 }
