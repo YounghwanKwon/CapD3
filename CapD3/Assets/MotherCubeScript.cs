f983d71d6ac0c23d4124bfa21691e59171c1d0d7
scullystory <kyh0124.12@gmail.com>
2019-10-06 13:27:28 +0900

    stage2_2 씬생성 , 두개의 프레팝추가 , 마더 큐브 스크립트추가

---
+++ CapD3/Assets/MotherCubeScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotherCubeScript : MonoBehaviour
+{
+    [SerializeField] private GameObject meshcube;
+    [SerializeField] private GameObject colicube;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            meshcube.SetActive(true);
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            meshcube.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshcube.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
