1f26319f419e2abc3b59e870a30d7ff6022f78f4
scullystory <kyh0124.12@gmail.com>
2019-10-09 19:02:47 +0900

    오른쪽 밸런스 수정 , 스리거 스탑, 레이저 데미지와 색상 세분화

--- CapD3/Assets/CMScript.cs
+++ CapD3/Assets/CMScript.cs
@@ -5,29 +5,51 @@ using UnityEngine;
 public class CMScript : MonoBehaviour
 {
     [SerializeField] private GameObject[] childrens;
+    [SerializeField] private GameObject centerbombpos;
     [SerializeField] private GameObject shell1;
+    [SerializeField] private GameObject shell2;
+    private bool needboom = true;
     private int i;
     private int j=0;
     // Start is called before the first frame update
     void Start()
     {
         i = (int)Random.Range(0f, 7f);
-        InvokeRepeating("randombomb", 2.5f, 4.5f);
+        Invoke("randombomb", 0.5f);
+        Invoke("centerbomb", 2.5f);
     }
     public void randombomb()
     {
-        i = (int)(Random.value*100);
-        j++;
-        i %= 8;
-        j %= 8;
-        drophere(childrens[i].transform);
-        drophere(childrens[j].transform);
+        if(needboom == true)
+        {
+            i = (int)(Random.value * 100);
+            j++;
+            i %= 8;
+            j %= 8;
+            drophere(childrens[i].transform);
+            drophere(childrens[j].transform);
+            Invoke("randombomb", 1.5f);
+        }
+    }
+    public void centerbomb()
+    {
+        if (needboom == true)
+        {
+            GameObject shell12 = Instantiate(shell2, centerbombpos.transform);
+            shell12.SetActive(true);
+            shell12.transform.parent = null;
+            Invoke("centerbomb", 5f);
+        }
     }
     public void drophere(Transform A)
     {
         GameObject shell11 =  Instantiate(shell1, A);
         shell11.SetActive(true);
         shell11.transform.parent = null;
+    }
+    public void needboomfalsefy()
+    {
+        needboom = false;
     }
 
     // Update is called once per frame
