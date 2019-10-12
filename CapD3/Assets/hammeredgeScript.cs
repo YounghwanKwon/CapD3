b8bed4bcbc909b111eee96fc33ea5e925b3bb4d3
scullystory <kyh0124.12@gmail.com>
2019-10-12 22:02:13 +0900

    해머에 스크립트까지 장착 (데스 + 데미지) ,큐브 사이즈 조절, 실린더에 리지드바디

---
+++ CapD3/Assets/hammeredgeScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hammeredgeScript : MonoBehaviour
+{
+    [SerializeField] private GameObject hammersshell;
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("1hit something :"+other);
+        if (other.CompareTag("Hammersetground"))
+        {
+            GameObject shell = Instantiate(hammersshell, this.transform);
+            //shell.transform.parent = null;
+            //Time.timeScale = 0;
+        }
+        else if(other.CompareTag("Player"))
+        {
+            Complete.TankHealth TH = other.GetComponent<Complete.TankHealth>();
+            TH.TakeDamage(100.0f);
+            //Time.timeScale = 0;
+        }
+        else
+            Debug.Log("2hit something :" + other);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hammer move");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
