b678c2fa9bb0e862cd2b43205a2c72585defa5c2
yoondohyun <ydh3434@daum.net>
2019-10-13 17:35:33 +0900

    임시

---
+++ CapD3/Assets/Summonctrl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Summonctrl : MonoBehaviour
+{
+    [SerializeField] private GameObject summonedobj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Summon", 0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Summon()
+    {
+        Transform SummonPoint = new GameObject().transform;
+        //Debug.Log(SummonPoint.transform.position);
+        SummonPoint.position = this.transform.position;
+        //Debug.Log(SummonPoint.transform.position);
+        GameObject Bomb = Instantiate(summonedobj, SummonPoint);
+        //Debug.Log("소환");
+        Bomb.SetActive(true);
+        Bomb.transform.parent = null;
+    }
+}
